b5342667d550dd98afd0ecaecb1ed1195fd4ba3f
Adrián Álvarez <adalsa91@gmail.com>
2016-10-20 22:51:21 +0200

    Minnor changes

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
 from flask import Flask
 
-
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-
 
 @app.route('/')
 def hello():
@@ -13,7 +11,7 @@ def hello():
 
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!".format(name)
+    return "Hola {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
