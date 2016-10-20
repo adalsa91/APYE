71a421377fc3803d13ec59b386eab3622c4c734a
Adrián Álvarez <adalsa91@gmail.com>
2016-10-20 10:16:10 +0200

    New requeriments

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = 'Sql1D00WTF.'
 
 
 class ProductionConfig(Config):
